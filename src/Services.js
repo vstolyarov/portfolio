6b72eeabd980651f7e3ed224ed437c08c76dbe35
Vlad Stolyarov <vstolyarov3@Vlads-iMac.local>
2018-09-19 16:33:35 -0400

    icons

--- src/Services.js
+++ src/Services.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './Services.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 class Services extends Component {
   render() {
     return (
       <div className="Services">
-        <h3>services</h3>
-        <h2>What we offer</h2>
+        <h3>Services</h3>
+        <h2>what we offer</h2>
+        <div className="row">
+            <div>
+                
+                Favorite Food: <FontAwesomeIcon icon={faCoffee} />
+
+                
+            </div>
+        </div>
       </div>
     );
   }
